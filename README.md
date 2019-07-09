c302ff607d4bc2d7ffb87b2603aa1427583f0a1a
Arturas V <larytis@gmail.com>
2019-07-09 11:00:01 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## IGT
+
+In order to get styles minified you must to run commands:
+
+### `gulp sass`
+
+ 
+  
+Start up server to load index.html
+
+### `node ./app/server.js`
---
+++ app/js/Main.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function(){
+    jQuery('.mobileMenuToggleButton').on('click', function(){
+        jQuery('.body').toggleClass('activeMobileMenu');
+    }); 
+});
